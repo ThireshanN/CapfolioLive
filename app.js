55908a3a7e3b33f0defdbaef24b2408feed97880
Kristen Coupe <kristen.coupe.02@gmail.com>
2023-04-01 17:45:55 +1300

    db stuff

--- app.js
+++ app.js
@@ -8,22 +8,31 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: "capfoliodb.cnducntmxm4l.ap-southeast-2.rds.amazonaws.com",
     user: "admin",
-    password: "admincapfolio",
+    password: "admincapfolio", //use environment variables
     port: "3306",
     database: "Capfolio"
 });
-connection.connect(function (err) {
-    if (err) {
-        console.error('Database failed to connect: ' + err.stack);
-        return;
-    }
-    console.log('Database successfully connected.');
-    const sql = "CREATE TABLE comments (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), message VARCHAR(255))";
-    connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Table created: " + result);
+function createTables() {
+    connection.connect(function (err) {
+        if (err) {
+            console.error('Database failed to connect: ' + err.stack);
+            return;
+        }
+        console.log('Database successfully connected.');
+        const sql = "CREATE TABLE comments (id INT AUTO_INCREMENT PRIMARY KEY, namee VARCHAR(255), message VARCHAR(255))";
+        connection.query(sql, function (err, result) {
+            if ("Table 'comments' already exists" == err.sqlMessage) {
+                console.log("table already exists! Try another sql statement like ALTER TABLE");
+                process.exit(1);
+                //console.log(err.stack);
+                //throw err;
+            } else {
+                console.log("Table created: " + result);
+            };
+        });
     });
-});
+}
+createTables(); //ideally call this function once
 //connection.end();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,4 +43,7 @@ app.get('/test', (req, res) => {
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { console.log(`App listening on port ${port}\nGo to http://localhost:3000 if testing locally\nGo to http://ec2-3-27-94-14.ap-southeast-2.compute.amazonaws.com:3000 if using on aws`); })
+app.listen(port, () => { console.log(`App listening on port ${port}\nGo to http://localhost:3000 if testing locally\nGo to http://ec2-3-27-94-14.ap-southeast-2.compute.amazonaws.com:3000 if using on aws`); });
+
+
+module.exports.connection = connection;
