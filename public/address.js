cf8ea1f48d381c5c0d31138c7a79fc7b743c5d51
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2023-04-13 15:12:20 +1200

    test1

--- public/address.js
+++ public/address.js
@@ -1,4 +1,4 @@
-const dnsAWS = 'ec2-3-25-252-237.ap-southeast-2.compute.amazonaws.com';
+const dnsAWS = 'ec2-3-27-105-138.ap-southeast-2.compute.amazonaws.com';
 
 module.exports.dnsAWS = dnsAWS;
 //export {dnsAWS}; 
