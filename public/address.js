4873d87674c46cdf58836536d3074c1fe6586349
ThireshanN <thireshannaidoo@gmail.com>
2023-04-15 15:00:56 +1200

    changing DNS

--- public/address.js
+++ public/address.js
@@ -1,4 +1,4 @@
-const dnsAWS = 'ec2-3-27-105-138.ap-southeast-2.compute.amazonaws.com';
+const dnsAWS = 'ec2-3-27-138-251.ap-southeast-2.compute.amazonaws.com';
 
 module.exports.dnsAWS = dnsAWS;
 //export {dnsAWS}; 
