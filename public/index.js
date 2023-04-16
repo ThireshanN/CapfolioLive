dc93f1034b706fcc7c92cb8046593d1a799863e6
ThireshanN <thireshannaidoo@gmail.com>
2023-04-16 18:20:26 +1200

    testing oauth2

--- public/index.js
+++ public/index.js
@@ -2,6 +2,23 @@ const dnsname = 'ec2-3-27-105-138.ap-southeast-2.compute.amazonaws.com';
 //const URL = `http://${dnsname}:3000/api`;
 const URL = 'http://localhost:3000/api';
 
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
 const ViewComments = async () => {
     //alert("Hello!\nYou will now view the comments");
     let allComments = "";
