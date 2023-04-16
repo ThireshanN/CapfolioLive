c60adea2628f999eb3198f96f0e229de7f667472
ThireshanN <thireshannaidoo@gmail.com>
2023-04-16 20:04:04 +1200

    fixing bug

--- public/index.js
+++ public/index.js
@@ -5,18 +5,18 @@ const URL = 'http://localhost:3000/api';
 
 
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
+    var auth2 = google.accounts.id.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
 }
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+function onSignIn(credential) {
+    var profile = credential.getBasicProfile();
+    console.log('ID: ' + profile.getId());
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('Email: ' + profile.getEmail());
 }
 
 const ViewComments = async () => {
