ee1c3ab447c4847e961b52378bf19314a34f087d
ThireshanN <thireshannaidoo@gmail.com>
2023-04-16 18:28:06 +1200

    updating things

--- public/address.js
+++ public/address.js
@@ -1,3 +1,3 @@
-export const dnsAWS = 'ec2-3-27-105-138.ap-southeast-2.compute.amazonaws.com';
+export const dnsAWS = 'ec2-3-27-128-129.ap-southeast-2.compute.amazonaws.com';
 
 //module.exports.dnsAWS = dnsAWS;
