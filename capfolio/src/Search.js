02c05f0fa54940d4a1a8abf5cf531eb7717ec9f6
josie <jche530@aucklanduni.ac.nz>
2023-04-20 01:41:48 +1200

    search bar changed to include button

--- capfolio/src/Search.js
+++ capfolio/src/Search.js
@@ -1,44 +1,53 @@
 import './Search.css';
 import { React, useState, classes } from "react";
 import TextField from "@mui/material/TextField";
-//import List from "./Components/List"
-//import "./App.css";
-import { createTheme } from '@mui/material/styles';
-import { indigo } from '@mui/material/colors';
+// import List from "./components/List"
+import "./App.css";
+// import { createTheme } from '@mui/material/styles';
+// import { indigo } from '@mui/material/colors';
+import SearchBar from './components/SearchBar';
 
-const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#42a5f5',
-      },
-      secondary: indigo,
-    },
-  })
+// const theme = createTheme({
+//     palette: {
+//       primary: {
+//         main: '#42a5f5',
+//       },
+//       secondary: indigo,
+//     },
+//   })
 
-const Search = () => (
-    <div className="search-bg">
-                <div className="illu"><img src={require('./images/capfolio-illu.png')}  alt="Logo" draggable='false'/></div>
-        <div className='headline'>A home dedicated to <br></br>showcasing bright futures</div>
-        <div className="webzen-logo"><p>Made by the team at</p><img src={require('./images/webzen-logo-white.png')}  alt="Logo" /></div>
+const Search = () => {
 
-        <div className="main">
-        <div className="search">
-            <TextField
-            id="outlined-basic"
-            variant="filled"
-            color="primary"
-            sx={{
-                backgroundColor : 'white',
-            }}
-            fullWidth
-            label="Search"
-            />
-        </div> 
-    </div>
+      const handleSearch = (searchTerm) => {
+        console.log('Search:', searchTerm);
+      };
+
+      return (
+        <div className="search-bg">
+                    <div className="illu"><img src={require('./images/capfolio-illu.png')}  alt="Logo" draggable='false'/></div>
+            <div className='headline'>A home dedicated to <br></br>showcasing bright futures</div>
+            <div className="webzen-logo"><p>Made by the team at</p><img src={require('./images/webzen-logo-white.png')}  alt="Logo" /></div>
+
+            <div className="main">
+              <div className="search"> 
+                <SearchBar onSearch={handleSearch} />
+                  {/* <TextField
+                  id="outlined-basic"
+                  variant="filled"
+                  color="primary"
+                  sx={{
+                      backgroundColor : 'white',
+                  }}
+                  fullWidth
+                  label="Search"
+                  /> */}
+              </div> 
+          </div>
+        
+        </div>
+      
+      );
+    };
     
-    </div>
-    
-  );
-
   export default Search;
   
