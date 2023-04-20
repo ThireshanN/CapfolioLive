98183bb8d99bef144f7ad20f242732d191553421
josie <jche530@aucklanduni.ac.nz>
2023-04-20 23:47:07 +1200

    made fairly fully responsive, other small css changes

--- capfolio/src/Search.js
+++ capfolio/src/Search.js
@@ -13,7 +13,7 @@ const Search = () => {
       return (
         <div className="search-bg">
                     <div className="illu"><img src={require('./images/capfolio-illu.png')}  alt="Logo" draggable='false'/></div>
-            <div className='headline'>A home dedicated to <br></br>showcasing bright futures</div>
+            <div className='headline'>A home dedicated to showcasing <span className='bright'>bright</span> futures</div>
             <div className="webzen-logo"><p>Made by the team at</p><img src={require('./images/webzen-logo-white.png')}  alt="Logo" /></div>
 
             <div className="main">
