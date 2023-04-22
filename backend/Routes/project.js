f3cc28aac78d23d1bae60e3e8154cf77482c46da
Kristen Coupe <kristen.coupe.02@gmail.com>
2023-04-22 21:42:54 +1200

    2 routes complete. FilteredProjectData and AllProjectData

--- backend/Routes/project.js
+++ backend/Routes/project.js
@@ -5,53 +5,120 @@ import { config } from '../sqlconfig.js';
 
 
 
-async function executeSQLstatement(sql) {
-    const connection = await mysql.createConnection(config.db);
-    const [rows, result] = await connection.execute(sql);
-    //console.log(rows, result);
-    await connection.end();
-    return JSON.stringify(rows);
+class ProjectSchema {
+    constructor(ProjectID = null, ProjectName = null, IsApproved = null, projectDec = null, capstoneYear = null, capstoneSemester = null, githubLink = null, adminID_FK = null, TeamName = null, VideoLink = null, ProjectIntro = null) {
+        this.ProjectID = ProjectID;
+        this.ProjectName = ProjectName;
+        this.IsApproved = IsApproved;
+        this.projectDec = projectDec;
+        this.capstoneYear = capstoneYear;
+        this.capstoneSemester = capstoneSemester;
+        this.githubLink = githubLink;
+        this.adminID_FK = adminID_FK;
+        this.TeamName = TeamName;
+        this.VideoLink = VideoLink;
+        this.ProjectIntro = ProjectIntro;
+    }
 }
 
-//generate code to get the scheme and save it in a class
-//create a schema/data model folder with the classes
-//then when node app.js, and a post route gets called, then check the req.body against it
-//that way, with every restart, we always get the correct schema/data model
-async function getProjectFields() {
+class ProjectSchema2 {
+    ProjectID;
+    ProjectName;
+    IsApproved;
+    projectDec;
+    capstoneYear;
+    capstoneSemester;
+    githubLink;
+    adminID_FK;
+    TeamName;
+    VideoLink;
+    ProjectIntro;
+    constructor() { }
+}
+
+
+async function executeSQLstatement(sql) { //working 22/04/2023 YUP
+    const connection = await mysql.createConnection(config.db);
+    const [rows, result] = await connection.execute(sql);
+    //console.log(rows, result); 
+    await connection.end();
+    return [rows, result];
+}
+
+async function ProjectSchemaAndFieldNames() { //working 22/04/2023 YUP
     try {
         const sql = "SHOW FIELDS FROM Capfolio.Project";
-        let fieldData = await executeSQLstatement(sql);
-        fieldData = JSON.parse(fieldData);
+        let fieldData = (await executeSQLstatement(sql))[0];
         const columnsArray = fieldData.map(element => element['Field']);
         const columnsString = columnsArray.join(", ");
-        console.log(fieldData);
-        console.log(columnsArray);
-        console.log(columnsArray.join(", "));
+        //console.log('\n\n',columnsArray, columnsString, '\n\n');
         return [columnsArray, columnsString];
-
     }
     catch (err) {
-        console.error("something went wrong in getProjectSchema()\nstart up the database on AWS console to fix this :)" + err);
+        console.error("something went wrong in ProjectSchemaAndFieldNames()\nstart up the database on AWS console to fix this " + err);
     }
 };
-getProjectFields();
+//ProjectSchemaAndFieldNames(); //working 22/04/2023 YUP
 
 
 
-//http://localhost:3000/project/projectData
-//http://ec2-3-26-95-151.ap-southeast-2.compute.amazonaws.com:3000/project/projectData
-projectRouter.get('/projectData', async (req, res) => {
+
+
+
+
+
+//http://localhost:3000/project/AllProjectData
+//http://ec2-3-26-95-151.ap-southeast-2.compute.amazonaws.com:3000/project/AllProjectData
+
+projectRouter.get('/AllProjectData', async (req, res) => { //working 22/04/2023 YUP
     try {
         const sql = "SELECT * FROM Capfolio.Project";
-        const allProjects = await executeSQLstatement(sql)//.catch(err => console.log("The following error generated:\n" + err));
+        const allProjects = (await executeSQLstatement(sql))[0]//.catch(err => console.log("The following error generated:\n" + err));
         console.log("Our Data: \n", allProjects);
         return res.status(200).setHeader("Content-Type", "application/json").send(allProjects);
     }
     catch (err) {
         console.log(err.message);
-        return res.status(200).setHeader("Content-Type", "text/plain").send("failed to fetch project data because of " + err);
+        return res.status(400).setHeader("Content-Type", "text/plain").send("failed to fetch project data because of " + err);
     }
 });
+
+
+
+//http://localhost:3000/project/FilteredProjectData
+//http://ec2-3-26-95-151.ap-southeast-2.compute.amazonaws.com:3000/project/FilteredProjectData
+
+projectRouter.get('/FilteredProjectData', async (req, res) => { //working 22/04/2023 YUP
+    try {
+        const regBodyFromClient = new ProjectSchema2();
+        regBodyFromClient.capstoneSemester = 2;
+        regBodyFromClient.capstoneYear = '2023';
+        const projectFields = (await ProjectSchemaAndFieldNames())[0];
+        let finalFilter = [];
+        projectFields.forEach(element => myFunction(element));
+        function myFunction(element) {
+            if (regBodyFromClient[`${element}`] !== undefined) {
+                finalFilter.push(`${element} = ${regBodyFromClient[`${element}`]}`);
+            }
+        }
+        finalFilter = finalFilter.join(' AND ');
+
+        const sql = `SELECT * FROM Capfolio.Project WHERE ${finalFilter}`;
+        const allProjects = (await executeSQLstatement(sql))[0]//.catch(err => console.log("The following error generated:\n" + err));
+        //console.log("Our Data: \n", allProjects);
+        console.log(allProjects);
+        if (allProjects.length === 0) {
+            throw new Error("There were no projects found");
+        }
+        console.log(typeof allProjects);
+        return res.status(200).setHeader("Content-Type", "application/json").send(allProjects);
+    }
+    catch (err) {
+        console.log(err.message);
+        return res.status(400).setHeader("Content-Type", "text/plain").send("Sorry! " + err);
+    }
+});
+
 
 
 //http://localhost:3000/project/AddProjectAllFields
@@ -84,7 +151,7 @@ function validateClientRequest(fieldsArray, data) {
         let newdata = fieldsArray.map(element => {
             return data.element;
         });
-        const valueString = "('" + newdata.join("', '") + "')"; 
+        const valueString = "('" + newdata.join("', '") + "')";
         return valueString;
     } else {
         throw new Error("Invalid client request");
@@ -95,11 +162,11 @@ function validateClientRequest(fieldsArray, data) {
 
 projectRouter.post('/AddProjectAllFields', express.json(), async (req, res) => {
     try {
-        const columnData = getProjectFields();
+        const columnData = ProjectSchemaAndFieldNames();
         const fields = columnData[1];
         const values = validateClientRequest(columnData[0], req.body.data); //either req.body or req.body.data
         const sql = `INSERT INTO Capfolio.Project (${fields}) VALUES (${values}))`;
-        const pdata = await executeSQLstatement(sql);
+        const pdata = (await executeSQLstatement(sql))[0];
         console.log("The Data: \n", pdata);
         return res.status(200).setHeader("Content-Type", "application/json").send(pdata);
     }
