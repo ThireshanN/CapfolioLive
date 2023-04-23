001485ecc8c1b161ce2bb13f55af4639b749b6cc
ThireshanUOA <tnai955@aucklanduni.ac.nz>
2023-04-24 00:10:05 +1200

    big updates

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -8,6 +8,37 @@ import mysql from 'mysql2/promise';
 import { config } from '../sqlconfig.js';
 
 
+async function executeSQLstatement(sql, values) {
+    const connection = await mysql.createConnection(config.db);
+    const [rows, result] = await connection.execute(sql, values);
+    await connection.end();
+    return [rows, result];
+}
+
+async function emailExists(email) {
+    const sql = `SELECT COUNT(*) as count FROM Users WHERE Email = ?;`;
+    const [rows] = await executeSQLstatement(sql, [email]);
+    return rows[0].count > 0;
+}
+
+async function next_id() {
+    const sql = `SELECT UserID FROM Users ORDER BY UserID DESC LIMIT 1;`;
+    const var1 = (await executeSQLstatement(sql));
+    //console.log(var1[0][0]);
+    //console.log(var1[0][0].UserID);
+    //console.log(Number(var1[0][0].UserID) + 1);
+    return Number(var1[0][0].UserID) + 1;
+}
+
+async function fetchUserData(email) {
+    const sql = `
+        SELECT Users.FirstName, Users.LastName, Users.Email, Users.UserTypeID as Type
+        FROM Users
+        WHERE Users.Email = ?;
+    `;
+    const [rows] = await executeSQLstatement(sql, [email]);
+    return rows[0];
+}
 
 const router = Router();
 
@@ -42,7 +73,48 @@ passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: 'http://localhost:3000/auth/google/callback'
-}, (accessToken, refreshToken, profile, cb) => {
+}, async (accessToken, refreshToken, profile, cb) => {
+
+    //const sql = `Insert into Visitors(UserID, UserTypeID, FirstName, lastName) values (40, 4, "Paul", "Pogba")`;
+
+    const emailToCheck = profile.emails[0].value;
+    const name = profile.displayName;
+    const sep_name = name.split(" ");
+    const first_name = sep_name[0];
+    const last_name = sep_name[1];
+    let type;
+    let sql;
+    let sql2;
+    let var1;
+    let var2;
+
+    const exists = await emailExists(emailToCheck);
+    //console.log("Email exists:", exists);
+    if (!exists) {
+        const id = await next_id();
+        if (emailToCheck === 'admin@aucklanduni.ac.nz') {
+            type = 3;
+        } else if (emailToCheck.endsWith('@aucklanduni.ac.nz')) {
+            type = 1;
+            sql = `Insert into Users(UserID, UserTypeID, FirstName, lastName, Email) values (${id}, ${type}, "${first_name}", "${last_name}", "${emailToCheck}");`;
+            sql2 = `Insert into Student(UserID, UserTypeID,  StudentUPI) values (${id}, 1, "${emailToCheck.substring(0,7)}");`;
+            const var1 = (await executeSQLstatement(sql));
+            const var2 = (await executeSQLstatement(sql2));
+        } else {
+            type = 4;
+            sql = `Insert into Users(UserID, UserTypeID, FirstName, lastName, Email) values (${id}, ${type}, "${first_name}", "${last_name}", "${emailToCheck}");`;
+            sql2 = `Insert into Visitor(UserID, UserTypeID) values (${id}, 4);`;
+            const var1 = (await executeSQLstatement(sql));
+            const var2 = (await executeSQLstatement(sql2));
+        }
+
+
+    }
+
+
+
+
+    //Insert into Admins(UserID) values (40);
 
     return cb(null, profile);
 }));
@@ -67,7 +139,18 @@ router.get('/user', async (req, res) => {
         }
 
         const user = req.user;
-        res.send(user);
+        const email = user.emails[0].value;
+        const sql = `SELECT u.FirstName, u.LastName, u.UserTypeID FROM Users u WHERE u.Email = ?;`;
+        const [rows] = await executeSQLstatement(sql, [email]);
+        const userData = rows[0];
+
+        res.send({
+            FirstName: userData.FirstName,
+            LastName: userData.LastName,
+            Email: email,
+            UserType: userData.UserTypeID,
+            Photo: user.photos[0].value
+        });
     } catch (error) {
         res.status(400).send({ error: 'Failed to fetch user data' });
     }
