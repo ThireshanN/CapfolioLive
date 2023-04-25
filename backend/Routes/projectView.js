713c8a0c8bd1fad010d21a5a8c3f427be06b3097
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2023-04-26 11:34:38 +1200

    likedProjects route added

--- backend/Routes/projectView.js
+++ backend/Routes/projectView.js
@@ -168,3 +168,49 @@ projectViewRouter.delete('/postDisLike', express.json(), async (req, res) => {
 
 
 
+//http://localhost:3000/projects/likedProjects
+//http://ec2-3-26-95-151.ap-southeast-2.compute.amazonaws.com:3000/projects/postLike
+
+
+projectViewRouter.get("/likedProjects", async (req, res) => { 
+    if(currentUserId===null){ return res.status(404).send("Only logged in Users can dislike");}
+    try {
+        const sql = `SELECT * 
+        FROM (
+        SELECT ProjectID, '0' AS hasLiked
+        FROM Project
+        WHERE ProjectID
+        not in (
+        SELECT ProjectID
+        FROM Users
+        INNER JOIN likes ON Users.UserId = likes.UserID_FK
+        INNER JOIN Project ON Project.ProjectID = likes.ProjectID_FK
+        WHERE UserID=43
+        GROUP BY Project.ProjectID)
+        UNION ALL
+        SELECT ProjectID, '1' AS hasLiked
+        FROM Users
+        INNER JOIN likes ON Users.UserId = likes.UserID_FK
+        INNER JOIN Project ON Project.ProjectID = likes.ProjectID_FK
+        WHERE UserID=${currentUserId}
+        GROUP BY Project.ProjectID) result
+        ORDER BY ProjectID;`;
+        const selectedProject = (await executeSQLstatement(sql))[0]//.catch(err => console.log("The following error generated:\n" + err));
+        //console.log("Our Data: \n", selectedProject);
+        if(selectedProject.length===0){
+            return res.status(404).send("likes not found"); //return 404 if project not found
+        }
+        else{
+        return res.status(200).setHeader("Content-Type", "application/json").send(selectedProject);
+        }
+    }
+    catch (err) {
+        console.log(err.message);
+        return res.status(400).setHeader("Content-Type", "text/plain").send("failed to fetch project data because of " + err);
+    }
+});
+
+
+
+
+
