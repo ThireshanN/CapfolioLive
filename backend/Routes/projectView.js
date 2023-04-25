36932d000d7e624d0f0668f271e8a0809730de5e
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2023-04-25 22:24:10 +1200

    comment error fixed

--- backend/Routes/projectView.js
+++ backend/Routes/projectView.js
@@ -3,7 +3,7 @@ export const projectViewRouter = express.Router();
 import mysql from 'mysql2/promise';
 import { config } from '../sqlconfig.js';
 
-async function executeSQLstatement(sql) { //working 23/04/2023
+async function executeSQLstatement(sql) { 
     const connection = await mysql.createConnection(config.db);
     const [rows, result] = await connection.execute(sql);
     await connection.end();
@@ -22,9 +22,11 @@ projectViewRouter.get("/project", async (req, res) => {
         const selectedProject = (await executeSQLstatement(sql))[0]//.catch(err => console.log("The following error generated:\n" + err));
         //console.log("Our Data: \n", selectedProject);
         if(selectedProject.length===0){
-            res.status(404).send("Item not found"); //return 404 if project not found
+            res.status(404).send("Project not found"); //return 404 if project not found
         }
-        return res.status(200).setHeader("Content-Type", "application/json").send(selectedProject);
+        else{
+            return res.status(200).setHeader("Content-Type", "application/json").send(selectedProject);
+        }
     }
     catch (err) {
         console.log(err.message);
@@ -42,11 +44,12 @@ projectViewRouter.get("/comment", async (req, res) => {
     try {
         const sql = `SELECT CommentDesc, FirstName, lastName, SUBSTRING(createdTime, 1, 10) AS createdTime, ProjectName, UserType FROM Comment INNER JOIN Users ON Comment.UserID_FK = Users.UserID  INNER JOIN Project ON Comment.ProjectID_FK = Project.ProjectID INNER JOIN UserType ON Users.UserTypeID = UserType.UserTypeID  WHERE ProjectID = ${projectId} ORDER BY CommentID DESC;`;
         const selectedProject = (await executeSQLstatement(sql))[0]//.catch(err => console.log("The following error generated:\n" + err));
-        //console.log("Our Data: \n", selectedProject);
         if(selectedProject.length===0){
-            res.status(404).send("Item not found"); //return 404 if project not found
+            res.status(404).send("No comments yet!"); //return 404 if project not found
         }
+        else{
         return res.status(200).setHeader("Content-Type", "application/json").send(selectedProject);
+        }
     }
     catch (err) {
         console.log(err.message);
@@ -65,9 +68,11 @@ projectViewRouter.get("/like", async (req, res) => {
         const selectedProject = (await executeSQLstatement(sql))[0]//.catch(err => console.log("The following error generated:\n" + err));
         //console.log("Our Data: \n", selectedProject);
         if(selectedProject.length===0){
-            res.status(404).send("Item not found"); //return 404 if project not found
+            res.status(404).send("likes not found"); //return 404 if project not found
         }
+        else{
         return res.status(200).setHeader("Content-Type", "application/json").send(selectedProject);
+        }
     }
     catch (err) {
         console.log(err.message);
