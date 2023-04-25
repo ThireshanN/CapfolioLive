aa041f7b9629c2cdd7b5264d574b558fc4e02baa
joshh <jhan234@aucklanduni.ac.nz>
2023-04-25 13:51:26 +1200

    Changes

--- capfolio/src/projectView.js
+++ capfolio/src/projectView.js
@@ -259,6 +259,7 @@ const ProjectView = () => {
                 <div className='comments'>
                     <div className='commentheading'>
                         <h2>Comments</h2>
+                        
                     </div>
                     <div className='writecomment'>
 
