ca77844159401f7958b73811db8cf6e6a0d27737
josie <jche530@aucklanduni.ac.nz>
2023-05-04 22:15:18 +1200

    banner icons and card shadow update

--- capfolio/src/components/awardBanner.js
+++ capfolio/src/components/awardBanner.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './awardStyles.css';
-import CommunityIcon from '../images/community.svg';
-import ExcellenceIcon from '../images/excellence.svg';
-import PeoplesChoiceIcon from '../images/peopleschoice.svg';
+import { TbAward } from 'react-icons/tb';
+import { TbMoodHeart } from 'react-icons/tb';
+import { TbHeartHandshake } from 'react-icons/tb';
 
 const AwardBanner = ({ text }) => {
     const getBannerStyle = (awardName) => {
@@ -10,17 +10,17 @@ const AwardBanner = ({ text }) => {
             case 'Community Impact Award':
                 return {
                     color: 'linear-gradient(270deg, #20A786 0%, #1ACB95 24.82%, rgba(133, 245, 94, 0.69) 93.44%, #64EE34 93.44%, #64EE34 93.44%)',
-                    icon: CommunityIcon
+                    icon: TbMoodHeart
                 };
             case 'Excellence Award':
                 return {
                     color:'linear-gradient(270deg, #EFA30E 0%, #FAB651 39.42%, #EEDA71 93.44%)',
-                    icon: ExcellenceIcon
+                    icon: TbAward
                 };
             case 'Peoples Choice Award':
                 return {
                     color: 'linear-gradient(270deg, #197FB8 4.87%, #2BA0E2 44.26%, #5EE3F5 93.44%)',
-                    icon: PeoplesChoiceIcon
+                    icon: TbHeartHandshake
                 };
             default:
                 return {
@@ -30,14 +30,14 @@ const AwardBanner = ({ text }) => {
         }
     };
 
-    const { color, icon } = getBannerStyle(text);
+    const { color, icon: Icon } = getBannerStyle(text);
 
     if (text === null) {
         return null;
     } else {
         return (
             <div className="award-banner" style={{ backgroundImage: color }}>
-                <img src={icon} alt="" style={{ marginRight: '4px' }} />
+                {Icon && <Icon style={{ marginRight: '5px', marginLeft: '5px', fontSize: '32px'}} />}
                 <span>{text}</span>
             </div>
         );
