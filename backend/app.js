a339953fe1e45fcc1af26fe290df7ad55e8f245c
Kristen Coupe <kristen.coupe.02@gmail.com>
2023-05-04 21:51:04 +1200

    fixed

--- backend/app.js
+++ backend/app.js
@@ -82,11 +82,11 @@ app.get('/test', (req, res) => {
 
 app.get('/*', (req, res) => { //http:localhost:3000/
     //res.sendFile(path.join(__dirname, '../clientdemo/build', index.html));
-    // res.sendFile(path.join(__dirname, '../capfolio/build/index.html'), function (err) {
-    //     if (err) {
-    //         res.status(500).send(err)
-    //     }
-    // })
+    res.sendFile(path.join(__dirname, '../capfolio/build/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
 })
 
 
--- capfolio/src/components/awardBanner.js
+++ capfolio/src/components/awardBanner.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './awardStyles.css';
-// import CommunityIcon from '../images/community.svg';
-// import ExcellenceIcon from '../images/excellence.svg';
-// import PeoplesChoiceIcon from '../images/peopleschoice.svg';
+import CommunityIcon from '../images/community.svg';
+import ExcellenceIcon from '../images/excellence.svg';
+import PeoplesChoiceIcon from '../images/peopleschoice.svg';
 
 const AwardBanner = ({ text }) => {
     const getBannerStyle = (awardName) => {
@@ -10,17 +10,17 @@ const AwardBanner = ({ text }) => {
             case 'Community Impact Award':
                 return {
                     color: 'linear-gradient(270deg, #20A786 0%, #1ACB95 24.82%, rgba(133, 245, 94, 0.69) 93.44%, #64EE34 93.44%, #64EE34 93.44%)',
-                    //icon: CommunityIcon
+                    icon: CommunityIcon
                 };
             case 'Excellence Award':
                 return {
                     color:'linear-gradient(270deg, #EFA30E 0%, #FAB651 39.42%, #EEDA71 93.44%)',
-                    //icon: ExcellenceIcon
+                    icon: ExcellenceIcon
                 };
             case 'Peoples Choice Award':
                 return {
                     color: 'linear-gradient(270deg, #197FB8 4.87%, #2BA0E2 44.26%, #5EE3F5 93.44%)',
-                    //icon: PeoplesChoiceIcon
+                    icon: PeoplesChoiceIcon
                 };
             default:
                 return {
