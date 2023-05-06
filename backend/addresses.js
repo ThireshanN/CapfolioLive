f85cd1d663d0d322c8ca41c1f8902019b26bc819
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2023-05-07 02:24:30 +1200

    new changes

--- backend/addresses.js
+++ backend/addresses.js
@@ -1,3 +1,3 @@
-export const dnsAWS = 'ec2-54-206-44-253.ap-southeast-2.compute.amazonaws.com';
+export const dnsAWS = ' ec2-54-79-153-66.ap-southeast-2.compute.amazonaws.com';
 
 //module.exports.dnsAWS = dnsAWS;
