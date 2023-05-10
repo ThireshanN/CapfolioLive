f3cff6dedf7c0e9ea2c96df820c232fb25d5dc1b
josie <jche530@aucklanduni.ac.nz>
2023-05-10 16:14:46 +1200

    more hompage and nav bar css changes

--- capfolio/src/Search.js
+++ capfolio/src/Search.js
@@ -12,7 +12,8 @@ const Search = ({ onApplySearch }) => {
 
   return (
     <div className="search-bg">
-      <div className="illu"><img src={require('./images/capfolio-illu.png')} alt="Logo" draggable='false' /></div>
+      <div className="illu"></div>
+      {/* <div className="illu"><img src={require('./images/home-imgv2.png')} alt="Logo" draggable='false' /></div> */}
       <div className='headline'>A home dedicated to showcasing <span className='bright'>bright</span> futures</div>
       <div className="webzen-logo"><p>Made by the team at WebZen</p></div>
       {/* <img src={require('./images/webzen-logo-black.png')} alt="Logo" /> */}
