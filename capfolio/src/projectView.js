0bf867d0eeb35b2d0303cfb905bcd80e6912bf37
joshh <jhan234@aucklanduni.ac.nz>
2023-05-11 12:31:37 +1200

    started changes for deleteing comments

--- capfolio/src/projectView.js
+++ capfolio/src/projectView.js
@@ -2,7 +2,7 @@ import { CButton } from '@coreui/react';
 import {
     MDBTextArea
 } from 'mdb-react-ui-kit';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
@@ -10,6 +10,8 @@ import LikeButton from "./components/likeButton";
 import avatar from './images/avatar.png';
 import gitHubLogo from './images/github-mark-white.png';
 import './projectView.css';
+import animationData from './images/icons8-trash.json';
+import lottie from 'lottie-web';
 
 import AWS from "aws-sdk";
 
@@ -28,15 +30,15 @@ const s3 = new AWS.S3({
 
 
 const ProjectView = () => {
-
     const [data, setData] = useState([]);
     const [responses, setResponses] = useState([]);
-
     const params = useParams();
     const [comments, setComments] = useState('');
     const [projects, setProject] = useState('');
     const [tech, setTech] = useState('');
+    const [user, setUser] = useState('');
     // const blueBoxRef = useRef();
+
 
 
     const getProject = async () => {
@@ -68,8 +70,6 @@ const ProjectView = () => {
 
     };
 
-
-
     useEffect(() => {
         const fetchResponses = async () => {
             const responseArray = [];
@@ -94,6 +94,13 @@ const ProjectView = () => {
         ).then((response) => response.json());
 
         setComments(response);
+
+        const userID = await fetch(
+            "/auth/user"
+        ).then((userID) => userID.json())
+
+        setUser(userID.UserID)
+        console.log(response)
     };
 
 
@@ -314,19 +321,45 @@ const ProjectView = () => {
 
                     <div className='showcomments'>
                         {comments &&
-                            comments.map((comment) => (
+                            comments.map((comment) => {
+                                if (comment.User === user) {
+                                    return (
+                                        <div className='comment'>
+                                            <svg class="icon-trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 40" width="40" height="40">
+                                                <path class="trash-lid" fill-rule="evenodd" d="M6 15l4 0 0-3 8 0 0 3 4 0 0 2 -16 0zM12 14l4 0 0 1 -4 0z" />
+                                                <path class="trash-can" d="M8 17h2v9h8v-9h2v9a2 2 0 0 1-2 2h-8a2 2 0 0 1-2-2z" />
+                                            </svg>
+                                            <div className='commentDetails'>
+                                                
+                                                <img className='comment-avatar' src={avatar} alt="avatar"></img>
+                                                <p className='commentname'>{comment.FirstName}</p>
 
-                                <div className='comment'>
-                                    <div className='commentDetails'>
-                                        <img className='comment-avatar' src={avatar} alt="avatar"></img>
-                                        <p className='commentname'>{comment.FirstName}</p>
+                                                <p className='commentUsertype'>{comment.UserType}</p>
+                                                <p className='commentdate'>{comment.createdTime}</p>
+                                            </div>
+                                            <p className='commenttext'>{comment.CommentDesc}</p>
 
-                                        <p className='commentUsertype'>{comment.UserType}</p>
-                                        <p className='commentdate'>{comment.createdTime}</p>
-                                    </div>
-                                    <p className='commenttext'>{comment.CommentDesc}</p>
-                                </div>
-                            ))}
+                                            
+
+                                        </div>
+                                    );
+                                }
+                                else {
+                                    return (
+                                        <div className='comment'>
+                                            <div className='commentDetails'>
+                                                <img className='comment-avatar' src={avatar} alt="avatar"></img>
+                                                <p className='commentname'>{comment.FirstName}</p>
+
+                                                <p className='commentUsertype'>{comment.UserType}</p>
+                                                <p className='commentdate'>{comment.createdTime}</p>
+                                            </div>
+                                            <p className='commenttext'>{comment.CommentDesc}</p>
+                                        </div>
+                                    );
+                                }
+
+                            })}
 
                     </div>
                 </div>
