2be9b3c5ce50334fbe65e426912a9a3e6b50d847
joshh <jhan234@aucklanduni.ac.nz>
2023-06-05 17:43:48 +1200

    Can update profile picture

--- capfolio/src/pages/Profile.js
+++ capfolio/src/pages/Profile.js
@@ -16,6 +16,7 @@ import ProjectIntro from "../components/ProjectIntro";
 import { ReactComponent as Heart } from "../images/heart.svg";
 import { ReactComponent as Views } from "../images/views.svg";
 import AwardBanner from "../components/awardBanner.js";
+import AWS from "aws-sdk";
 
 import {
   CCard,
@@ -32,19 +33,31 @@ import { Link } from "react-router-dom";
 import MainImage from "../components/getMainImage";
 import "./Profile.css";
 
+const bucketName = "capfoliostorage";
+const bucketRegion = "ap-southeast-2";
+const accessKeyId = "AKIAUDUQU75VEF3VDCEL";
+const secretAccessKey = "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";
+
+const s3 = new AWS.S3({
+  region: bucketRegion,
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+});
+
 const Profile = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [name, setName] = useState("");
   const [profession, setProfession] = useState("");
   const [github, setGithub] = useState("");
   const [linkedin, setLinkedin] = useState("");
-  const [description, setDescription] = useState("")
-  
+  const [description, setDescription] = useState("");
+  const [imgURL, setImgURL] = useState("");
+  const [newFile, setNewFile] = useState([]);
   const [picture, setPicture] = useState("");
   const [projects, setProjects] = useState([]);
   const [userProject, setUserProject] = useState([]);
   const [isStudent, setStudent] = useState(false);
-
+  const [userid, setUserId] = useState("");
   useEffect(() => {
     const fetchUserData = async () => {
       let userData;
@@ -54,12 +67,14 @@ const Profile = () => {
           throw new Error("User not authenticated");
         }
         userData = await response.json();
+        console.log(userData.Photo);
         setName(`${userData.FirstName} ${userData.LastName}`);
         setPicture(userData.Photo);
         setProfession(userData.Type);
         setGithub(userData.githubLink);
         setLinkedin(userData.linkedin);
         setDescription(userData.userDescription);
+        setUserId(userData.UserID);
         if (userData.UserType === 1) {
           setStudent(true);
         }
@@ -104,7 +119,9 @@ const Profile = () => {
     setIsEditing(true);
   };
 
-  const handleSaveProfile = () => {
+  const handleSaveProfile = async () => {
+    let createURL;
+    
     console.log({
       name: name,
       profession: profession,
@@ -116,21 +133,56 @@ const Profile = () => {
 
     const [firstName, lastName] = name.split(" ");
 
-    fetch("/profile/updateUser", {
-      method: "PUT",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        linkedin: linkedin,
-        github: github,
-        userDesc: description,
-        profession: profession,
-      }),
+    const highResPromises = newFile.map(async (file) => {
+      console.log(file);
+      const filename = file.name;
+      const key =
+        "" + "assets" + "/" + "profilePictures" + "/" + userid + "/" + filename;
+      createURL =
+        "https://capfoliostorage.s3.ap-southeast-2.amazonaws.com/assets/profilePictures/" +
+        userid +
+        "/" +
+        filename;
+      setImgURL(createURL);
+      
+      const Imgparams = {
+        Bucket: bucketName,
+        Key: key,
+        ACL: "public-read",
+        Body: file,
+      };
+
+      try {
+        const data = await s3.upload(Imgparams).promise();
+        return data;
+      } catch (err) {
+        console.error(err);
+      }
     });
+
+    try {
+      await Promise.all(highResPromises);
+      setPicture(createURL);
+
+      await fetch("/profile/updateUser", {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName: firstName,
+          lastName: lastName,
+          linkedin: linkedin,
+          github: github,
+          userDesc: description,
+          profession: profession,
+          Picture: createURL ? createURL : picture,
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
 
     setIsEditing(false);
   };
@@ -153,6 +205,19 @@ const Profile = () => {
   const handleLinkedinChange = (e) => {
     setLinkedin(e.target.value);
   };
+  const handlePictureChange = async (file) => {
+    const reader = new FileReader();
+
+    reader.onload = async () => {
+      console.log(reader.result);
+      const newPictureSrc = reader.result;
+      setPicture(newPictureSrc);
+    };
+    reader.readAsDataURL(file);
+    const fileArray = [];
+    fileArray.push(file);
+    setNewFile(fileArray);
+  };
 
   return (
     <section style={{ backgroundColor: "#eee" }}>
@@ -161,13 +226,35 @@ const Profile = () => {
           <MDBCol lg="4">
             <MDBCard className="mb-4">
               <MDBCardBody className="text-center">
-                <MDBCardImage
-                  src={picture}
-                  alt="avatar"
-                  className="rounded-circle"
-                  style={{ width: "150px" }}
-                  fluid
-                />
+                {isEditing ? (
+                  <div>
+                    <form action="/action_page.php">
+                      <input
+                        type="file"
+                        id="myFile"
+                        name="filename"
+                        onChange={(e) => handlePictureChange(e.target.files[0])}
+                      ></input>
+                    </form>
+
+                    <MDBCardImage
+                      src={picture}
+                      alt="avatar"
+                      className="rounded-circle"
+                      style={{ width: "150px", height: "150px" }}
+                      fluid
+                    />
+                  </div>
+                ) : (
+                  <MDBCardImage
+                    src={picture}
+                    alt="avatar"
+                    className="rounded-circle"
+                    style={{ width: "150px", height: "150px" }}
+                    fluid
+                  />
+                )}
+
                 {isEditing ? (
                   <div>
                     <input
