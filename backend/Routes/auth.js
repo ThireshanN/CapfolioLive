d0a0f9c9971de6d1ffee1a34964356a5c605bb16
Kristen Coupe <kcou558@aucklanduni.ac.nz>
2023-06-13 18:03:40 +1200

    fix google auth

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -135,7 +135,7 @@ const googleAuth = (req, res, next) => {
       {
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
+        callbackURL: "https://capfolio.live/auth/google/callback"
       },
       async (accessToken, refreshToken, profile, cb) => {
         //const sql = `Insert into Visitors(UserID, UserTypeID, FirstName, lastName) values (40, 4, "Paul", "Pogba")`;
